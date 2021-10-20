258c8a39f87d50142149bf7e3531e1351983c59d
Felipe Roberto Sumiya <felipersumiya@gmail.com>
2021-10-20 18:18:50 -0300

    Criação da entidade Order e mapeamento de relações no BD

--- src/main/java/com/felipersumiya/course/entities/User.java
+++ src/main/java/com/felipersumiya/course/entities/User.java
@@ -1,11 +1,14 @@
 package com.felipersumiya.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class User implements Serializable{
@@ -20,6 +23,8 @@ public class User implements Serializable{
 	private String email;
 	private String password;
 	
+	@OneToMany (mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 		
@@ -83,6 +88,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders(){
+		return orders;
 	}
 
 
