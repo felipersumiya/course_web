404446e66650b70daecacdb39ffc615b8a87fac3
Felipe Roberto Sumiya <felipersumiya@gmail.com>
2021-10-25 10:15:50 -0300

    Tratamento de erro para deleção de usuários.

--- src/main/java/com/felipersumiya/course/services/UserService.java
+++ src/main/java/com/felipersumiya/course/services/UserService.java
@@ -4,10 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.felipersumiya.course.entities.User;
 import com.felipersumiya.course.repositories.UserRepository;
+import com.felipersumiya.course.services.exceptions.DatabaseException;
+import com.felipersumiya.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -25,7 +29,7 @@ public class UserService {
 	public User findById(Long id){
 		
 		Optional<User> obj = userRepository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
@@ -36,7 +40,17 @@ public class UserService {
 	
 	public void delete(Long id) {
 		
-		userRepository.deleteById(id);
+		try {
+			
+			userRepository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+			
+			
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update (Long id, User user) {
